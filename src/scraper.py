7a63c8e3f824fd04e61bba7cc36e98644e1224ac
hs <haukesteffen@mbp.lan>
2024-09-24 21:48:25 +0200

    Small refactor #3

--- src/scraper.py
+++ src/scraper.py
@@ -11,7 +11,11 @@ from time import perf_counter
 
 
 class Scraper:
-    def __init__(self, batch_size=1000, verbose=False):
+    def __init__(
+            self,
+            batch_size: int = 1000, 
+            verbose: bool = False
+            ):
         self.verbose = verbose
         self.batch_size = batch_size
         self._empty_dicts()
@@ -19,6 +23,13 @@ class Scraper:
         self.engine = create_engine(f'postgresql://{os.environ["DBUSER"]}:{os.environ["DBPW"]}@localhost:5432/hndb')
         self.max_id = self._get_max()
         self.last_id = self._get_last()
+        self.type_mapping = {
+            'story': ['id', 'title', 'by', 'descendants', 'score', 'time', 'url'],
+            'job': ['id', 'title', 'text', 'by', 'score', 'time', 'url'],
+            'comment': ['id', 'text', 'by', 'time', 'parent'],
+            'poll': ['id', 'title', 'text', 'by', 'descendants', 'score', 'time'],
+            'pollopt': ['id', 'text', 'by', 'poll', 'score', 'time'],
+        }
         if self.verbose:
             print(f'Initialized scraper. Last ID: {self.last_id}, Max ID: {self.max_id}, Batch Size: {self.batch_size}')
 
@@ -30,7 +41,6 @@ class Scraper:
             self.jsons = await self._scrape_batch(self.ids)
             for json in self.jsons:
                 self._to_dict(json)
-            self._convert_dicts()
             self._insert_sql()
             self.last_id = self._get_last()
             self.max_id = self._get_max()
@@ -132,13 +142,28 @@ class Scraper:
         url = 'https://hacker-news.firebaseio.com/v0/maxitem.json'
         response = self.client.get(url)
         return int(response.text)
+    
+    def _sanitize_text(self, text: str):
+        try:
+            return html.unescape(text).replace('\\x00','').replace('\x00','')
+        except TypeError:
+            return np.nan
+    
+    def _append_data(self, data_store, type, **kwargs):
+        if type in self.type_mapping and type in data_store:
+            fields = self.type_mapping[type]
+            store = data_store[type]
+
+            for field in fields:
+                if field in kwargs:
+                    store[field].append(kwargs[field])
 
     def _to_dict(self, json):
+        id = json.get('id')
+        type = json.get('type')
+
         # sanity check
-        try:
-            id = json['id']
-            type = json['type']
-        except KeyError:
+        if type == None:
             self.skipped.append(id)
             return
         
@@ -146,137 +171,64 @@ class Scraper:
         self.scrape['id'].append(id)
         self.scrape['scrape_time'].append(datetime.now())
 
-        # check if deleted
-        try:
-            if json['deleted']:
-                self.deleted['item'].append(json['id'])
-        except KeyError:
-            pass
+        # check if deleted or dead
+        if json.get('deleted'):
+            self.deleted['item'].append(id)
+        if json.get('dead'):
+            self.dead['item'].append(id)
 
-        # check if dead
-        try: 
-            if json['dead']:
-                self.dead['item'].append(json['id'])
-        except KeyError:
-            pass
-
+        title = self._sanitize_text(json.get('title'))
+        text = self._sanitize_text(json.get('text'))
+        by = json.get('by')
+        score = json.get('score')
+        time = json.get('time')
         try:
-            title = json['title']
-            title = html.unescape(title)
-            title = title.replace('\\x00','')
-        except KeyError:
-            title = np.nan
-
-        try:
-            text = json['text']
-            text = html.unescape(text)
-            text = text.replace('\x00','')
-        except KeyError:
-            text = np.nan
-
-        try:
-            by = json['by']
-        except KeyError:
-            by = np.nan
-        
-        try:
-            score = json['score']
-        except KeyError:
-            score = np.nan
-            
-        try:
-            time = datetime.fromtimestamp(int(json['time']))
-        except KeyError:
+            time = datetime.fromtimestamp(int(time))
+        except TypeError:
             time = np.nan
+        url = json.get('url')
+        descendants = json.get('descendants')
+        poll = json.get('poll')
+        parent = json.get('parent')
 
-        try:
-            url = json['url']
-        except KeyError:
-            url = np.nan
+        data_store = {
+            'story': self.stories,
+            'job': self.jobs,
+            'comment': self.comments,
+            'poll': self.polls,
+            'pollopt': self.pollopts,
+        }
 
-        try:
-            descendants = json['descendants']
-        except KeyError:
-            descendants = np.nan
-
-        try:
-            poll = json['poll']
-        except KeyError:
-            poll = np.nan
-
-        try:
-            parent = json['parent']
-        except KeyError:
-            parent = np.nan
-
-        if type == 'story':
-            self.stories['id'].append(id)
-            self.stories['title'].append(title)
-            self.stories['by'].append(by)
-            self.stories['descendants'].append(descendants)
-            self.stories['score'].append(score)
-            self.stories['time'].append(time)
-            self.stories['url'].append(url)
-
-        elif type == 'job':
-            self.jobs['id'].append(id)
-            self.jobs['title'].append(title)
-            self.jobs['text'].append(text)
-            self.jobs['by'].append(by)
-            self.jobs['score'].append(score)
-            self.jobs['time'].append(time)
-            self.jobs['url'].append(url)
-
-        elif type == 'comment':
-            self.comments['id'].append(id)
-            self.comments['text'].append(text)
-            self.comments['by'].append(by)
-            self.comments['time'].append(time)
-            self.comments['parent'].append(parent)
-
-        elif type == 'poll':
-            self.polls['id'].append(id)
-            self.polls['title'].append(title)
-            self.polls['text'].append(text)
-            self.polls['by'].append(by)
-            self.polls['descendants'].append(descendants)
-            self.polls['score'].append(score)
-            self.polls['time'].append(time)
-
-        elif type == 'pollopt':
-            self.pollopts['id'].append(id)
-            self.pollopts['text'].append(text)
-            self.pollopts['by'].append(by)
-            self.pollopts['poll'].append(poll)
-            self.pollopts['score'].append(score)
-            self.pollopts['time'].append(time)
-
-    def _convert_dicts(self):
-        self.scrape_df = pd.DataFrame(self.scrape)
-        self.skipped_df = pd.DataFrame(self.skipped)
-        self.deleted_df = pd.DataFrame(self.deleted)
-        self.dead_df = pd.DataFrame(self.dead)
-        self.stories_df = pd.DataFrame(self.stories)
-        self.jobs_df = pd.DataFrame(self.jobs)
-        self.comments_df = pd.DataFrame(self.comments)
-        self.polls_df = pd.DataFrame(self.polls)
-        self.pollopts_df = pd.DataFrame(self.pollopts)
+        self._append_data(
+            data_store=data_store,
+            type=type,
+            id=id,
+            title=title,
+            by=by,
+            text=text,
+            descendants=descendants,
+            score=score,
+            time=time,
+            url=url,
+            parent=parent,
+            poll=poll
+        )
 
     def _insert_sql(self):
         with self.engine.begin() as con:
-            self.scrape_df.to_sql(name='scrape', con=con, if_exists='append', index=False)
-            self.skipped_df.to_sql(name='skipped', con=con, if_exists='append', index=False)
-            self.deleted_df.to_sql(name='deleted', con=con, if_exists='append', index=False)
-            self.dead_df.to_sql(name='dead', con=con, if_exists='append', index=False)
-            self.stories_df.to_sql(name='stories', con=con, if_exists='append', index=False)
-            self.jobs_df.to_sql(name='jobs', con=con, if_exists='append', index=False)
-            self.comments_df.to_sql(name='comments', con=con, if_exists='append', index=False)
-            self.polls_df.to_sql(name='polls', con=con, if_exists='append', index=False)
-            self.pollopts_df.to_sql(name='pollopts', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.scrape).to_sql(name='scrape', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.skipped).to_sql(name='skipped', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.deleted).to_sql(name='deleted', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.dead).to_sql(name='dead', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.stories).to_sql(name='stories', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.jobs).to_sql(name='jobs', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.comments).to_sql(name='comments', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.polls).to_sql(name='polls', con=con, if_exists='append', index=False)
+            pd.DataFrame(self.pollopts).to_sql(name='pollopts', con=con, if_exists='append', index=False)
 
 
 async def main():
-    scraper = Scraper(batch_size=10000, verbose=True)
+    scraper = Scraper(batch_size=100000, verbose=True)
     await scraper.begin_scraping()
 
 if __name__ == '__main__':
