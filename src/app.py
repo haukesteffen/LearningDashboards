e64cd7db3b26208db12d5da10ed0932f32a0c3dc
hs <haukesteffen@mbp.lan>
2024-09-25 11:38:52 +0200

    Add table to dash app #5

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
-from dash import Dash, html
+import os
+import pandas as pd
+from dash import Dash, html, dash_table
+from sqlalchemy import create_engine
 
+# Incorporate data
+engine = create_engine(f'postgresql://{os.environ["DBUSER"]}:{os.environ["DBPW"]}@localhost:5432/hndb')
+with engine.begin() as con:
+    df = pd.read_sql('SELECT * FROM comments ORDER BY random() LIMIT 5', con=con)
+
+# Initialize the app
 app = Dash()
 
+# App layout
 app.layout = html.Div([
-    html.Div(children='Hello World')
-    ])
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
