9b88571da7e953cf6ba416c14150dd4198ca099a
hs <haukesteffen@mbp.local>
2024-09-28 19:22:17 +0200

    Add GET endpoint for greet function #16

--- src/api.py
+++ src/api.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/{name}")
+async def greet(name: str):
+    return {"message": f"Hello, {name.capitalize()}!"}
