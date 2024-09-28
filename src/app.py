3d5f35ad003a86ee967d4238209eb24e73e999ae
hs <haukesteffen@mbp.local>
2024-09-28 20:36:08 +0200

    Add dynamic data in dash app #19

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import pandas as pd
 import plotly.express as px
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
@@ -6,13 +7,6 @@ import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 from sqlalchemy import create_engine
 
-# Incorporate data
-engine = create_engine(f'postgresql://{os.environ["DBUSER"]}:{os.environ["DBPW"]}@localhost:5432/hndb')
-with engine.begin() as con:
-    df = pd.read_sql('SELECT * FROM comments ORDER BY random() LIMIT 5', con=con)
-    df = df[df['text'].str.len() != 0]
-    df['n_words'] = df['text'].apply(lambda x: len(x.split()))
-    df['n_cap'] = df['text'].apply(lambda x: len([word for word in x.split() if word.isupper()]))
 
 # Initialize the app
 app = Dash(external_stylesheets=[dbc.themes.LUX])
@@ -22,45 +16,31 @@ load_figure_template('LUX')
 # App layout
 app.layout = html.Div([
     html.Div(
-        children='My First App with Data'
+        children='My First App with Dynamic Data'
     ),
-    html.Div(
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            page_size=10,
-            style_cell={
-                'textOverflow': 'ellipsis',
-                'maxWidth': 0
-            })
-    ),
-    html.Div([
-        dcc.Dropdown(options=['n_words', 'n_cap'], value='n_words', id='dropdown1'),
-        dcc.Graph(figure={}, id='graph1'),
-        ], style={'width': '49%', 'display': 'inline-block'}
-    ), 
-    html.Div([
-        dcc.Dropdown(options=['n_words', 'n_cap'], value='n_words', id='dropdown2'),
-        dcc.Graph(figure={}, id='graph2'),
-        ], style={'width': '49%', 'display': 'inline-block'}
-    ),
+    dcc.Input(id="comment-input", type="text", placeholder="Input Comment ID"),
+    dash_table.DataTable(
+        id='comment-table',
+        page_size=10,
+        style_cell={
+            'textOverflow': 'ellipsis',
+            'maxWidth': 0
+        })
     ])
 
 # Add controls to build the interaction
-@callback(
-    Output(component_id='graph1', component_property='figure'),
-    Input(component_id='dropdown1', component_property='value')
+@app.callback(
+    Output('comment-table', 'data'),
+    Input('comment-input', 'value')
 )
-def update_graph1(col_chosen):
-    fig = px.scatter(df, x='time', y=col_chosen)
-    return fig
-
-@callback(
-    Output(component_id='graph2', component_property='figure'),
-    Input(component_id='dropdown2', component_property='value')
-)
-def update_graph2(col_chosen):
-    fig = px.scatter(df, x='time', y=col_chosen)
-    return fig
+def display_user_data(comment_id):
+    if comment_id:
+        # Fetch the selected user's data from FastAPI
+        response = requests.get(f"http://localhost:8000/comment/{comment_id}")
+        if response.status_code == 200:
+            comment_data = response.json()
+            return [comment_data]
+    return []
 
 # Run the app
 if __name__ == '__main__':
