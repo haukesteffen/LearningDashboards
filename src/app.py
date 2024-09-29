467a84c4ece2a8bcd6e0f57840bd7fa5cf6e4e1a
hs <haukesteffen@mbp.local>
2024-09-29 12:32:01 +0200

    Add dynamic query parameters using dash slider

--- src/app.py
+++ src/app.py
@@ -14,13 +14,17 @@ app.layout = html.Div([
     html.Div(
         children='My First App with Dynamic Data'
     ),
-    html.Div(
-        dcc.Input(id="comment-input", type="text", placeholder="Input Comment ID")
-    ),
+    html.Div([
+        dcc.Input(id="comment-input", type="text", placeholder="Input Comment ID"),
+        dcc.Slider(5, 25, 5,
+               value=15,
+               id='limit-slider'
+        )
+    ]),
     html.Div(
         dash_table.DataTable(
             id='comment-table',
-            page_size=10,
+            page_size=25,
             style_cell={
                 'textOverflow': 'ellipsis',
                 'maxWidth': 0
@@ -33,15 +37,16 @@ app.layout = html.Div([
 # Add controls to build the interaction
 @app.callback(
     Output('comment-table', 'data'),
-    Input('comment-input', 'value')
+    Input('comment-input', 'value'),
+    Input('limit-slider', 'value')
 )
-def display_user_data(comment_id):
+def display_user_data(comment_id, limit):
     if comment_id:
         # Fetch the selected user's data from FastAPI
-        response = requests.get(f"http://localhost:8000/comment/{comment_id}")
+        response = requests.get(f"http://localhost:8000/comment/{comment_id}/?limit={limit}")
         if response.status_code == 200:
             comment_data = response.json()
-            return [comment_data]
+            return comment_data
     return []
 
 # Run the app
