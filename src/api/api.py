6c8e1076efcf657cfa63285395ead142ce8ae968
hs <haukesteffen@mbp.local>
2024-09-29 11:21:44 +0200

    Add api endpoint that accepts parameter #20

--- src/api/api.py
+++ src/api/api.py
@@ -17,9 +17,9 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/comment/{comment_id}", response_model=schemas.CommentBase)
-def read_comment(comment_id: int, db: Session = Depends(get_db)):
-    db_comment = crud.get_comment(db, comment_id=comment_id)
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-    return db_comment
+@app.get("/comment/{comment_id}", response_model=list[schemas.CommentBase])
+def read_comment(comment_id: int, db: Session = Depends(get_db), limit: int = 5):
+    db_comments = crud.get_n_comments(db=db, comment_id=comment_id, limit=limit)
+    if db_comments is None:
+        raise HTTPException(status_code=404, detail="Comments not found")
+    return db_comments
--- src/api/utils/crud.py
+++ src/api/utils/crud.py
@@ -1,5 +1,13 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 from . import models
 
 def get_comment(db: Session, comment_id: int):
     return db.query(models.Comment).filter(models.Comment.id == comment_id).first()
+
+def get_n_comments(db: Session, comment_id: int, limit: int):
+    return db.query(models.Comment) \
+            .filter(models.Comment.id <= comment_id) \
+            .order_by(desc(models.Comment.id)) \
+            .limit(limit=limit) \
+            .all()
